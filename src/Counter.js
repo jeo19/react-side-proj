847624c3b64391933d7874c2b85a19a45c485809
jeo19 <terrakkk@outlook.com>
2021-04-26 19:06:01 +0800

    feat:Add the usage an exampe useState hook step01

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <p>
+        The current couter value is <b>{counter}</b>
+      </p>
+      <button onClick={() => setCounter(counter + 1)}>add 1+</button>
+      <button onClick={() => setCounter(counter - 1)}>del 1-</button>
+    </div>
+  );
+};
+export default Counter;
