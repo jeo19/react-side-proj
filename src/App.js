b3b221988901d38b63ecfe7a07314b6a5e229d49
jeo19 <terrakkk@outlook.com>
2021-04-28 10:05:40 +0800

    feat:Check the activity of useEffect Hook before the component is unmounted or updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './Info';
 
 function App() {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? 'Hidden' : 'Visible'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
