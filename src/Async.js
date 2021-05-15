25a8abc51f50b9e7c3ee07cdd276cbd03916adbb
jeo19 <terrakkk@outlook.com>
2021-05-15 10:41:24 +0800

    feat:Implement the asynchronous with setTimeout function (step02)

--- src/Async.js
+++ src/Async.js
@@ -1,11 +1,14 @@
-function work() {
+function work(callback) {
   setTimeout(() => {
     const start = Date.now();
     for (let i = 1; i < 100000; i += 1) {}
     const end = Date.now();
     console.log(`${end - start}ms`);
+    callback();
   }, 0);
 }
 console.log('Start work');
-work();
+work(() => {
+  console.log('work done');
+});
 console.log('next work');
