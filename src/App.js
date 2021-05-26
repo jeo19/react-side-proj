478755604c6a76c96bc11837e39422bc05f982ec
jeo19 <terrakkk@outlook.com>
2021-05-26 17:52:24 +0800

    feat:Create a variable in the component with useRef step01

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from 'react';
 import UserList from './UserList';
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: '1',
+      name: 'Alina',
+      email: 'alina@gmail.com',
+    },
+    {
+      id: '2',
+      name: 'pablo',
+      email: 'pablo@outlook.com',
+    },
+    {
+      id: '3',
+      name: 'Liana',
+      email: 'liana@yandex.com',
+    },
+  ];
+  return <UserList users={users} />;
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -8,28 +8,11 @@ function User({ user }) {
     </div>
   );
 }
-function UserList() {
-  const users = [
-    {
-      id: '1',
-      name: 'Alina',
-      email: 'alina@gmail.com',
-    },
-    {
-      id: '2',
-      name: 'pablo',
-      email: 'pablo@outlook.com',
-    },
-    {
-      id: '3',
-      name: 'Liana',
-      email: 'liana@yandex.com',
-    },
-  ];
+function UserList({ users }) {
   return (
     <div>
-      {users.map((user,index) => (
-        <User user={user}  key={index}/>
+      {users.map((user, index) => (
+        <User user={user} key={index} />
       ))}
     </div>
   );
