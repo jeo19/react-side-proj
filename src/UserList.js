af0d26c988e7c5135307f40b81fbfe5929722245
jeo19 <terrakkk@outlook.com>
2021-06-01 10:47:55 +0800

    feat:Implement counting of the active users with useMemo Hook step02

--- src/UserList.js
+++ src/UserList.js
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function User({ user, onRemove, onToggle }) {
-  useEffect(() => {
-    console.log('display a component');
-    console.log(user);
-    return () => {
-      console.log('disappear');
-      console.log(user);
-    };
-  }, [user]);
   return (
     <div>
       <b
