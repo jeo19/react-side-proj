d71c4ab5743e29584a335be71137d4aaf24694dc
jeo19 <terrakkk@outlook.com>
2021-06-16 09:40:44 +0800

    refact:Update the input logic to the custom useInputs hooks step01

---
+++ src/hooks/useInputs.js
@@ -0,0 +1,12 @@
+import { useState, useCallback } from 'react';
+
+function useInputs(initialFrom) {
+  const [form, setForm] = useState(initialFrom);
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
+  const reset = useCallback(() => {}, [initialFrom]);
+  return [form, onChange, reset];
+}
+export default useInputs;
